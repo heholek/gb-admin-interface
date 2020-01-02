86522285ed2f400379137c6dd311e5e1d0b2ddf3
Caelin Sutch <Caelinsutch@gmail.com>
2020-01-01 17:22:17 -0800

    Stylelint

--- src/app/pages/miscellaneous/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/pages/miscellaneous/terms-and-conditions/terms-and-conditions.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-terms-and-conditions',
   templateUrl: './terms-and-conditions.component.html',
-  styleUrls: ['./terms-and-conditions.component.scss'],
 })
 export class TermsAndConditionsComponent implements OnInit {
 
